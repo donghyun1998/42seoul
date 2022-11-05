36424991a718b22eaf2d7fb7f05d9c36377a3839
donghyun1998 <donghyun1998@naver.com>
2022-11-05 18:14:41 +0900

    add va-end

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,11 @@
 /*   By: donghyk2 <donghyk2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 13:37:13 by donghyk2          #+#    #+#             */
-/*   Updated: 2022/11/04 01:41:09 by donghyk2         ###   ########.fr       */
+/*   Updated: 2022/11/04 01:46:49 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-
-
 
 int	print_ret_cnt(const char *s, va_list ap)
 {
@@ -43,7 +41,11 @@ int	print_ret_cnt(const char *s, va_list ap)
 int	ft_printf(const char *s, ...)
 {
 	va_list	ap;
+	int		cnt;
 
+	cnt = 0;
 	va_start(ap, s);
-	return (print_ret_cnt(s, ap));
+	cnt == print_ret_cnt(s, ap);
+	va_end(ap);
+	return (cnt);
 }
