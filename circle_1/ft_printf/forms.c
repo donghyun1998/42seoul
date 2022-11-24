3f3ed2771d88736c39b5ddfc2777229be144e76a
donghyun1998 <donghyun1998@naver.com>
2022-11-24 17:20:43 +0900

    mod param

--- circle_1/ft_printf/forms.c
+++ circle_1/ft_printf/forms.c
@@ -6,7 +6,7 @@
 /*   By: donghyk2 <donghyk2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 20:56:18 by donghyk2          #+#    #+#             */
-/*   Updated: 2022/11/22 17:14:04 by donghyk2         ###   ########.fr       */
+/*   Updated: 2022/11/24 17:06:32 by donghyk2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	form_p(unsigned long long p)
 	return (write (1, "0x", 2) + ft_print_ptr_ret_cnt(p));
 }
 
-int	form_diu(long long c, int u_flag)
+int	form_diu(long long c, int uflag)
 {
-	if (u_flag)
+	if (uflag)
 		return (ft_putnbr_ret_cnt(c, 0));
 	else
 		return (ft_putnbr_ret_cnt((int)c, 0));
